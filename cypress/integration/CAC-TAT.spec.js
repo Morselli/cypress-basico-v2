1e14ab4b92b6883c5cdd9c455ae15841ce3d7a2a
Vinicius Morselli <vinicius_morselli@hotmail.com>
2023-11-09 23:06:09 -0300

    done all exercises from section 3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -16,7 +16,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('#open-text-area').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra ex quis dapibus accumsan. Maecenas elementum vitae ante at pulvinar. Maecenas faucibus, nisi et interdum volutpat, ipsum massa tempus leo, eget ultricies neque lectus vel ante.', {
       delay: 0
     })
-    cy.get('.button').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.success').should('be.visible')
   })
@@ -26,7 +26,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('#lastName').type('Morselli')
     cy.get('#email').type('vinicius-morselli.mail.com')
     cy.get('#open-text-area').type('Lorem ipsum')
-    cy.get('.button').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
@@ -37,7 +37,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       .should('have.value', '')
   })
 
-  it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
     cy.get('#firstName').type('Vinicius')
     cy.get('#lastName').type('Morselli')
     cy.get('#email').type('vinicius-morselli@mail.com')
@@ -45,8 +45,43 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('#open-text-area').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra ex quis dapibus accumsan. Maecenas elementum vitae ante at pulvinar. Maecenas faucibus, nisi et interdum volutpat, ipsum massa tempus leo, eget ultricies neque lectus vel ante.', {
       delay: 0
     })
-    cy.get('.button').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
+  })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+    cy.get('#firstName')
+      .type('Vinicius')
+      .should('have.value', 'Vinicius')
+      .clear()
+      .should('have.value', '')
+    cy.get('#lastName')
+      .type('Morselli')
+      .should('have.value', 'Morselli')
+      .clear()
+      .should('have.value', '')
+    cy.get('#email')
+      .type('vinicius-morselli@mail.com')
+      .should('have.value', 'vinicius-morselli@mail.com')
+      .clear()
+      .should('have.value', '')
+    cy.get('#phone')
+      .type('123456789')
+      .should('have.value', '123456789')
+      .clear()
+      .should('have.value', '')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('envia o formuário com sucesso usando um comando customizado', function() {
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
   })
 })
